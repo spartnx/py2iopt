a7d21a481994a84a70ec0ba9ea26840ca07aee29
Tristan SARTON DU JONCHAY <tristan-sarton@hotmail.fr>
2023-05-03 16:39:00 -0400

    Start implementing TwoImpulseRDV class

--- py2iopt/_two_impulse_rdv.py
+++ py2iopt/_two_impulse_rdv.py
@@ -7,18 +7,45 @@ import pygmo as pg
 from ._optim import deltav_udp
 
 class TwoImpulseRDV:
-    def __init__(self, algo="ipopt", mu=1):
-        """Define algo and dynamics environments"""
-        self.algo = algo
+    def __init__(self, mu=1, algo="ipopt"):
+        """Define dynamics environment and algorithm.
+        
+        Args:
+            mu (float): central body's gravitational parameter
+            algo (str): optimization algorithm
+        """
         self.mu = mu
+        self.algo = algo
         return
 
-    def setProblem(self):
-        """Define initial and final conditions"""
+    def setProblem(self, t0, tf, r0, rf, v0, vf):
+        """Define initial and final conditions of the rendezvous.
+        
+        Args:
+            t0 (float): time window lower bound
+            tf (float): time window upper bound
+            r0 (tuple): position vector at t0
+            rf (tuple): position vector at tf
+            v0 (tuple): velocity vector at t0
+            vf (tuple): velocity vector at tf
+        """
+        self.t0 = t0
+        self.tf = tf
+        self.r0 = r0
+        self.rf = rf
+        self.v0 = v0
+        self.vf = vf
         return
 
     def solve(self):
-        """Define UDP, UDA, population, and evolve()"""
+        """Minimize two-impulse rendezvous deltaV."""
+        # Define Pygmo problem
+        params = (self.t0, self.tf, self.r0, self.rf, self.v0, self.vf)
+        prob = pg.problem(udp=deltav_udp(params))
+        print(prob)
+
+        # Define Pygmo algorithm
+        
         return
 
     def plot_trajectory_3d(self):
